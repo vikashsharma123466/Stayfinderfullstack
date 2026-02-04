f4d50b01057db27998f1f8ba31e7523d7912b3d8
vikashsharma123466 <vikash26122003sharma@gmail.com>
2026-02-04 22:59:41 +0530

    apii

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://stayfinderfullstack.onrender.com/api",
+  baseURL: "https://stayfinderfullstack.onrender.com",
   withCredentials: true,
 });
 
