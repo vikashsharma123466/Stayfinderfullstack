23b0d352b57471ada7644399f3383168a996c3bd
vikashsharma123466 <vikash26122003sharma@gmail.com>
2026-02-04 23:10:48 +0530

    cc

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.MODE === 'development'
+  ? 'http://localhost:5000/api'
+  : 'https://stayfinderfullstack.onrender.com/api';
+
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Update this with your backend URL
+  baseURL: BASE_URL,
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
